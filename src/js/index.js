37295f8dc307b5a6a59cb5b237b50d21e418af30
Kenny <kennethichoi@gmail.com>
2018-11-24 18:19:01 -0800

    Completed search functionality w/ pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,49 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from "./views/base";
+
+/*****************
+ * Global state of app
+// - Search object
+// - Current recipe object
+// - Shopping list object
+// - Liked recipes
+*****************/
+const state = {}
+
+const controlSearch = async () => {
+    // 1. get query from view
+    const query = searchView.getInput(); // TODO
+    console.log(query);
+    // 2. 
+    if(query) {
+        // New search object and add to state
+        state.search = new Search(query);
+
+        // Prepare UI to show data
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // Search for recipes
+        await state.search.getResults();
+
+        // Render results in UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
---
+++ src/js/models/Search.js
@@ -0,0 +1,26 @@
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        const apiKey = 'eabea68c864b0ffc5d8474c5eaec6679--';
+        
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${apiKey}&q=${this.query}`);
+            this.result = res.data.recipes;
+            console.log(this.result);
+        } catch(error) {
+            console.log(error);
+        }
+        
+    }
+}
+
+import axios from 'axios';
+// API Key: eabea68c864b0ffc5d8474c5eaec6679
+
+// https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=2 
+
+
+
